0c5645424d5aa3efcf2838f29fe6f17cbafbebd9
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-08-17 15:44:12 -0500

    wip

--- apps/dashboard/app/(root)/calendar/page.tsx
+++ apps/dashboard/app/(root)/calendar/page.tsx
@@ -1,11 +1,3 @@
-"use client";
-
-import { useAbility } from "@/hooks/use-ability";
-
 export default function CalendarPage() {
-	const ability = useAbility();
-
-	console.log(ability.can("manage", "organization"));
-
 	return <div>Calendar</div>;
 }
