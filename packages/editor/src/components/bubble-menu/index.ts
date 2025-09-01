ce67f47a7731977454740b3b95ce5bceb4da74a1
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-09-01 15:52:50 -0500

    wip

--- packages/editor/src/components/bubble-menu/index.ts
+++ packages/editor/src/components/bubble-menu/index.ts
@@ -1,6 +1,6 @@
 export { FloatingBubbleMenu } from "./floating-bubble-menu";
 export { LinkPopover } from "./link-popover";
 export { NodeSelector } from "./node-selector";
-export { TextColorPicker } from "./text-color-picker";
+
 // Deprecated - use NodeSelector instead
 export { TurnIntoDropdown } from "./turn-into-dropdown";
